0f2b52b43213add061dad7628b5f740761f44361
BRPersonal <balajirengan@gmail.com>
2025-08-22 22:42:37 +0530

    minor code changes

--- extra_assignment1.py
+++ extra_assignment1.py
@@ -1,9 +1,9 @@
 
-def print_instructor() -> None:
+def print_institute() -> None:
     print("HOPE AI")
 
 def print_instructor_got_from_input() -> None:
-    instructor = input("Enter your institute name? : ")
+    instructor = input("Enter your instructor name? : ")
     print(instructor)
 
 def prompt_for_details() -> None:
@@ -14,44 +14,51 @@ def prompt_for_details() -> None:
     print(f"name={name},age={age},school={schoolname},degree={degree}")
 
 def add() -> None:
+    print("Adding two numbers")
     a = int(input("enter number 1:"))
     b = int(input("enter number 2:"))
     total = a + b
     print(f"a={a}\nb={b}\nadd={total}")
 
 def subtract() -> None:
+    print("Subtracting two numbers")
     a = int(input("enter number 1:"))
     b = int(input("enter number 2:"))
     diff = a - b
     print(f"a={a}\nb={b}\nsub={diff}")
 
 def multiply() -> None:
+    print("Multiplying two numbers")
     a = int(input("enter number 1:"))
     b = int(input("enter number 2:"))
     product = a * b
     print(f"a={a}\nb={b}\nmul={product}")
 
 def divide() -> None:
+    print("Performing float Division on two numbers")
     a = int(input("enter number 1:"))
     b = int(input("enter number 2:"))
     result = a / b
     print(f"a={a}\nb={b}\nFloat Div ={result}")
 
 def floor_division() -> None:
+    print("Performing floor division on two numbers")
     a = int(input("enter number 1:"))
     b = int(input("enter number 2:"))
     result = a // b
     print(f"a={a}\nb={b}\nFloor Div ={result}")
 
 def modulo() -> None:
+    print("Performing modulo on two numbers")
     a = int(input("enter number 1:"))
     b = int(input("enter number 2:"))
     result = a % b
     print(f"a={a}\nb={b}\nModulo ={result}")
 
 def power() -> None:
-    a = int(input("enter number 1:"))
-    b = int(input("enter number 2:"))
+    print("Performing x to the power of y")
+    a = int(input("enter number x:"))
+    b = int(input("enter number y:"))
     result = a ** b
     print(f"a={a}\nb={b}\nPower ={result}")
 
@@ -60,7 +67,7 @@ def power() -> None:
 
 
 if __name__ == "__main__":
-    print_instructor()
+    print_institute()
     print_instructor_got_from_input()
     prompt_for_details()
     add()
