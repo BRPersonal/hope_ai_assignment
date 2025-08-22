72e4658a901738c5e367a969b9a2c593519fd178
BRPersonal <balajirengan@gmail.com>
2025-08-22 22:50:31 +0530

    few code changes

--- extra_assignment2.py
+++ extra_assignment2.py
@@ -1,37 +1,43 @@
 def print_vertical(upper_bound:int) -> None:
     for i in range(upper_bound):
         print(i)
+    print("-" * 20)
 
 def print_horizontal(starting_number:int, upper_bound:int) -> None:
     for i in range(starting_number,upper_bound):
         print(i, end = " ")
     print("")
+    print("-" * 20)
 
 def print_no_of_items(lst:list) -> None:
-    print("Number of Items in the List:\n",len(lst))
+    print("Number of Items in the List:",len(lst))
+    print("-" * 20)
 
 def print_text_vertical(text: str) -> None:
     for c in text:
         print(c)
+    print("-" * 20)
 
 def print_tuple(tup: tuple) -> None:
-    print("-" * 10)
     for item in tup:
         if (isinstance(item,tuple)):
             print_tuple(item)  #recursive call
         else:
             print(f"{item}({type(item)})")
+    print("-" * 20)
 
 def print_odd_numbers(lst: list) -> None:
     for n in lst:
         if (n % 2 != 0):
             print(f"{n} is Odd")
+    print("-" * 20)
 
 def print_even_numbers(lst: list) -> None:
     for n in lst:
         if (n % 2 == 0):
             print(f"{n} is Even")
 
+    print("-" * 20)
 
 if __name__ == "__main__":
     print_vertical(20)
