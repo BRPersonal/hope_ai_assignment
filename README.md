3cabf660e000810fb5c85afd1c86eb85cd075361
BRPersonal <balajirengan@gmail.com>
2025-09-21 20:33:07 +0530

    refactored best model finding method

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # hope_ai_assignment
 Has python code for assignments given in hope AI course
+
+$ conda activate sse-env
+jupyter notebook was not working. Ran the following command
+$ conda install jupyter
+$ conda install numpy pandas
+$ jupyter notebook
+
+Conda is asking for more packages to install.
+So not proceeding with it for now
+$ conda deactivate
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from itertools import product
+from sklearn.metrics import r2_score
+
 
 def center_text(text:str,column_width:int) -> str:
 
@@ -14,3 +16,39 @@ def center_text(text:str,column_width:int) -> str:
 
 def generate_parameter_combinations(param_dict:dict) -> [dict] :
     return [dict(zip(param_dict.keys(),combo)) for combo in product(*param_dict.values())]
+
+def find_best_hyperparameters(param_dict, x_train, y_train, x_test, y_test, 
+                            create_regressor_callback, print_combo_callback)-> dict:
+    """
+    Find the best hyperparameter combination using callbacks.
+    
+    Args:
+        param_dict: Dictionary of various combinations of parameter values to try
+        x_train, y_train: Training data
+        x_test, y_test: Test data
+        create_regressor_callback: Lambda that takes combo dict and returns a regressor
+        print_combo_callback: Lambda that takes combo dict and prints it
+    
+    Returns:
+        best_combo: Dictionary containing the best parameter combination with r_score
+    """
+    
+    combinations = generate_parameter_combinations(param_dict)
+    
+    best_combo = None
+    max_r_score = float('-inf')
+    
+    for combo in combinations:
+        regressor = create_regressor_callback(combo)
+        regressor.fit(x_train, y_train)
+        y_predict = regressor.predict(x_test)
+        r_score = r2_score(y_test, y_predict)
+        combo["r_score"] = r_score
+        
+        print_combo_callback(combo)
+        
+        if r_score > max_r_score:
+            max_r_score = r_score
+            best_combo = combo.copy()
+    
+    return best_combo
